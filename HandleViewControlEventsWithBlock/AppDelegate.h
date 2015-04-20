c322197107433b8206b3bfed4943296891f0261f
pifupro_iOS_admin <chenyl@pifupro.com>
2015-04-21 00:44:37 +0800

    Initial Commit

---
+++ HandleViewControlEventsWithBlock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HandleViewControlEventsWithBlock
+//
+//  Created by chenyilong on 15/4/21.
+//  Copyright (c) 2015年 chenyilong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HandleViewControlEventsWithBlock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HandleViewControlEventsWithBlock
+//
+//  Created by chenyilong on 15/4/21.
+//  Copyright (c) 2015年 chenyilong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
